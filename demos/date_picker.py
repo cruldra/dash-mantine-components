e36c8272eb26f0e4c96a81b2f3889cd50e9f7834
cruldra <cruldra@gmail.com>
2024-08-04 13:12:38 +0800

    1
    #time 37m

---
+++ demos/date_picker.py
@@ -0,0 +1,33 @@
+from datetime import date, datetime
+
+from dash import html, callback, Output, Input
+from dash.exceptions import PreventUpdate
+
+import dongjak_dash_components as dmc
+
+simple_ui = html.Div(
+    [
+        dmc.DatePicker(
+            id="date-picker-input",
+            label="Start Date",
+            description="You can also provide a description",
+            minDate=date(2020, 8, 5),
+            value=datetime.now().date(),  # or string in the format "YYYY-MM-DD"
+            w=250,
+        ),
+        dmc.Space(h=10),
+        dmc.Text(id="selected-date-input"),
+    ]
+)
+
+
+@callback(
+    Output("selected-date-input", "children"),
+    Input("date-picker-input", "value")
+)
+def update_output(d):
+    prefix = "You have selected: "
+    if d:
+        return prefix + d
+    else:
+        raise PreventUpdate
--- usage.py
+++ usage.py
@@ -1,15 +1,113 @@
-import dongjak_dash_components as ddc
-from dash import Dash
+import os
+from datetime import date, datetime
 
-app = Dash(__name__)
+from dash import Dash, html, dcc, callback, Output, Input
+import dongjak_dash_components as dmc
+from dash_iconify import DashIconify
 
-app.layout = ddc.MantineProvider(
-    ddc.Alert(
-        "Hi from Dash Mantine Components. You can create some great looking dashboards using me!",
-        title="Welcome!",
-        color="violet",
-    )
+from demos.date_picker import simple_ui as date_picker_simple_ui
+
+
+def get_icon(icon):
+    return DashIconify(icon=icon, height=16)
+
+
+stylesheets = [
+    "https://unpkg.com/@mantine/dates@7/styles.css",
+    "https://unpkg.com/@mantine/code-highlight@7/styles.css",
+    "https://unpkg.com/@mantine/charts@7/styles.css",
+    "https://unpkg.com/@mantine/carousel@7/styles.css",
+    "https://unpkg.com/@mantine/notifications@7/styles.css",
+    "https://unpkg.com/@mantine/nprogress@7/styles.css",
+]
+scripts = [
+    "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.8/dayjs.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.8/locale/zh-cn.min.js",
+    # "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.8/locale/ru.min.js",
+]
+app = Dash(__name__, external_stylesheets=stylesheets, external_scripts=scripts, suppress_callback_exceptions=True)
+server = app.server
+menus = html.Div(
+    style={"width": 240,
+           "background": "#313131",
+           },
+    children=[
+
+        dmc.NavLink(
+            label="日期时间",
+            leftSection=get_icon(icon="iconoir:user"),
+            children=[
+                dmc.NavLink(
+                    label="日期选择框",
+                    leftSection=get_icon(icon="iconoir:user"),
+                    href="/date-picker",
+                )
+            ],
+        )
+    ],
 )
 
-if __name__ == "__main__":
-    app.run_server()
+main_content = html.Div(
+    style={"flex": 1,
+
+           # "border": "1px solid",
+           },
+    children=dmc.DatesProvider(
+        id="main-content",
+        settings={"locale": "zh-cn", "firstDayOfWeek": 0, "weekendDays": [0]}
+    ),
+
+)
+
+app.layout = dmc.MantineProvider(
+    forceColorScheme="dark",
+    theme={
+        "primaryColor": "indigo",
+        "fontFamily": "'Inter', sans-serif",
+        "components": {
+            "Button": {"defaultProps": {"fw": 400}},
+            "Alert": {"styles": {"title": {"fontWeight": 500}}},
+            "AvatarGroup": {"styles": {"truncated": {"fontWeight": 500}}},
+            "Badge": {"styles": {"root": {"fontWeight": 500}}},
+            "Progress": {"styles": {"label": {"fontWeight": 500}}},
+            "RingProgress": {"styles": {"label": {"fontWeight": 500}}},
+            "CodeHighlightTabs": {"styles": {"file": {"padding": 12}}},
+            "Table": {
+                "defaultProps": {
+                    "highlightOnHover": True,
+                    "withTableBorder": True,
+                    "verticalSpacing": "sm",
+                    "horizontalSpacing": "md",
+                }
+            },
+        },
+    },
+    children=[
+        dmc.NotificationProvider(position="top-right"),
+        dcc.Location(id='url', refresh=False),
+        html.Div(
+            style={
+                "display": "flex",
+                "direction": "row",
+                "margin": "20px",
+                "gap": "20px",
+
+            },
+            children=[menus, main_content]
+        )
+    ]
+
+)
+
+
+@callback(
+    Output('main-content', 'children'),
+    Input('url', 'pathname')
+)
+def display_page(pathname):
+    if pathname == '/date-picker':
+        return date_picker_simple_ui
+
+
+if __name__ == '__main__':
+    app.run(debug=os.getenv("DEBUG", "True").lower() == "true")
